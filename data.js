27ecf14cdc90402c571bbec42d929b03469d7ee8
Aser333 <114796203+Aser333@users.noreply.github.com>
2022-12-17 17:00:22 +1000

    Add files via upload

---
+++ data.js
@@ -0,0 +1,64 @@
+const usedCommentIds = [];
+const usedPostIds = [];
+  
+const generateCommentId = () => {
+    let commentId = getRandomIntInclusive(0, 250);
+    while (usedCommentIds.includes(commentId)) {
+        commentId = getRandomIntInclusive(0, 250)
+    }
+
+    usedCommentIds.push(commentId);
+    
+    return commentId;
+};
+
+const generateComment = () => {
+    return {
+        id: generateCommentId(),
+        avatar: `./img/avatar-${getRandomIntInclusive(1, 6)}.jpg`,
+        message: getRandomArrayElement(MESSAGES),
+        name: getRandomArrayElement(NAMES),
+    };
+}
+
+
+const generatePostId = () => {
+    let usedPostId = getRandomIntInclusive(0, 250);
+    while (usedPostIds.includes(usedPostId)) {
+        usedPostId = getRandomIntInclusive(0, 250)
+    };
+
+    usedPostIds.push(usedPostId);
+
+    return usedPostId;
+};
+
+const generatePost = () => {
+    const comments = [];
+
+    let i = 0;
+    while (i < getRandomIntInclusive (0, 10)) {
+        comments.push(generateComment());
+        i++;
+    };
+
+    return {
+        id: generatePostId(),
+        url: `./photos/${getRandomIntInclusive(1, POST_COUNT)}.jpg`,
+        description: getRandomArrayElement(DESCRIPTIONS),
+        likes: 15,
+        comments: comments,
+    };
+};
+
+const generatePosts = (count) => {
+    const posts = [];
+
+    for (let i = 0; i < count; i++) {
+        posts.push(generatePost());
+    }
+
+    return posts;
+};
+
+console.log(generatePosts(26));
